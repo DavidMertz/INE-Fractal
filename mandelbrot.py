04a0c1c0a516e73680376d9d9d27be9269d9e25d
Git Learner <student@example.com>
2020-12-29 05:25:33 +0000

    Add doctest to mandelbrot

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,4 +1,13 @@
 def mandelbrot(z0:complex, orbits:int=255) -> int:
+    """Find the escape orbit of points under Mandelbrot iteration
+    
+    >>> mandelbrot(0.0965-0.638j)
+    17
+    
+    # Might need to increase orbits to resolve some points
+    >>> mandelbrot(0.106225-0.6376125j, orbits=1000)
+    291
+    """
     z = z0
     for n in range(orbits):
         if abs(z) > 2.0:
@@ -6,4 +15,5 @@ def mandelbrot(z0:complex, orbits:int=255) -> int:
         z = z * z + z0
         
 if __name__ == '__main__':
-    print(mandelbrot(0.0965-0.638j))  # Should print 17
+    import doctest
+    doctest.testmod()
